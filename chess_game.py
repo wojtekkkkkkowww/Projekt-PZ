9cffdd1dfc07d662a4d25f6f3ea7c8a562c4f428
wojtekkkkkkowww <268486@student.pwr.edu.pl>
2023-10-28 19:10:46 +0200

    "Aded pone promotion"

--- chess_game.py
+++ chess_game.py
@@ -18,14 +18,28 @@ class ChessGame(tk.Frame):
         self.board = chess.Board()
         self.canvas.draw_board(self.board)
     
+    def enforce_promotion(self,move, board, promotion_piece):
+        if move.promotion is None:
+            from_square = move.from_square
+            to_square = move.to_square
+            if board.piece_at(from_square) == chess.Piece(chess.PAWN, board.turn):
+                promotion_rank = 7 if board.turn == chess.WHITE else 0
+                if chess.square_rank(to_square) == promotion_rank:
+                    move.promotion = promotion_piece
+        return move
+
+
     def make_move(self,to_square):
         if self.canvas.highlighted_square is not None:
             move = chess.Move(self.canvas.highlighted_square, to_square)
+            move = self.enforce_promotion(move, self.board, chess.QUEEN)
+
             if move in self.board.legal_moves:
                 self.board.push(move)
                 self.canvas.highlighted_square = None
                 self.current_color = not self.current_color
                 self.canvas.draw_board(self.board)
+           
             else:
                 self.canvas.unhighlight_square()
                 self.canvas.highlighted_square = None
